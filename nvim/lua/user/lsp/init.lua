f7eebff846198078de7ae59d0e46af4243b413ef
gonzaless <gonzaless.7@gmail.com>
2022-10-23 23:04:23 -0500

    2022-10-24 04:04:23 backup

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -81,6 +81,26 @@ local function on_attach(client, bufnr)
 
     -- Enable completion triggered by <C-x><C-O>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+
+    -- Server capabilities spec:
+    -- https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#serverCapabilities
+    --if client.server_capabilities.documentHighlightProvider then
+        --vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+        --vim.api.nvim_clear_autocmds { buffer = bufnr, group = "lsp_document_highlight" }
+        --vim.api.nvim_create_autocmd("CursorHold", {
+            --callback = vim.lsp.buf.document_highlight,
+            --buffer = bufnr,
+            --group = "lsp_document_highlight",
+            --desc = "Document Highlight",
+        --})
+        --vim.api.nvim_create_autocmd("CursorMoved", {
+            --callback = vim.lsp.buf.clear_references,
+            --buffer = bufnr,
+            --group = "lsp_document_highlight",
+            --desc = "Clear All the References",
+        --})
+    --end
 end
 
 
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -90,10 +90,39 @@ return packer.startup(function(use)
     ---------------------------------------------------------------------------
     use 'lukas-reineke/indent-blankline.nvim'
 
+--[[
     use {'nvim-treesitter/nvim-treesitter', run = function()
-        require('nvim-treesitter.install').update({ with_sync = true })
-    end}
+        require('nvim-treesitter.install').update {
+            with_sync = true
+        }
+        require'nvim-treesitter.configs'.setup {
+            ensure_installed = { 'c', 'cmake', 'cpp', 'javascript', 'json', 'lua', 'python', 'rust' },
+            highlight = {
+                enable = true, -- `false` will disable the whole extension
 
+                -- Disable selected languages
+                --disable = { "c", "rust" },
+                -- Or use a function for more flexibility, e.g. to disable slow treesitter hgighlight for large files
+                --disable = function(lang, buf)
+                    --local max_filesize = 100 * 1024 -- 100 KB
+                    --local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    --if ok and stats and stats.size > max_filesize then
+                        --return true
+                    --end
+                --end,
+
+                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                -- Instead of true it can also be a list of languages
+                additional_vim_regex_highlighting = false,
+            },
+            indent = {
+                enabled = true,
+            },
+        }
+    end}
+--]]
 
     ---------------------------------------------------------------------------
     -- LSP
