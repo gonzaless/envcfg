112ca47ab497b9305cb805dc73d18e1dc801a313
gonzaless <gonzaless.7@gmail.com>
2023-02-10 18:37:21 -0600

    Support automatic deployment

--- backup.sh
+++ backup.sh
@@ -1,50 +1,166 @@
 #!/usr/bin/env sh
 
+
+error() {
+    echo "$@" 1>&2
+}
+
+fatal_error() {
+    echo "$@" 1>&2
+    exit 1
+}
+
+print_help() {
+    echo "Usage: ${0##*/} [--backup | --deploy | --help]"
+}
+
+
+# Parse command line
+if [[ $# > 1 ]]; then
+  error "Single action needs to be specified"
+  print_help
+  exit 1
+fi
+
+action=backup
+
+while [[ $# > 0 ]]; do
+    case $1 in
+        -b|--backup)
+            shift
+            ;;
+        -d|--deploy)
+            action=deploy
+            shift
+            ;;
+        -h|--help)
+            print_help
+            exit 0
+            ;;
+        *)
+            error "Unknown option $1"
+            print_help
+            exit 1
+            ;;
+    esac
+done
+
+
+# Utils
 repo_root=`git rev-parse --show-toplevel`
 
-# Copy changes
-echo Backing up current config into $repo_root
+cfg_sync_begin() {
+    echo ""
+    echo "Syncing $1 ..."
+}
+
+cfg_sync_end() {
+    echo "Done"
+}
+
+set_dir() {
+    config_dir=${1/#\~/$HOME}
+    case $# in
+        1)
+            backup_dir=${repo_root}/${1##*/}
+            ;;
+        2)
+            backup_dir=$2
+            ;;
+        *)
+            fatal_error "Invalid ${FUNCNAME[0]} argument number $#"
+            ;;
+    esac
+
+    if [[ -z $backup_dir || -z $config_dir ]]; then
+        fatal_error "Invalid ${FUNCNAME[0]} arguments: $@"
+    fi
+
+    case $action in
+        'backup')
+            src_root=$config_dir
+            dst_root=$backup_dir
+            ;;
+        'deploy')
+            src_root=$backup_dir
+            dst_root=$config_dir
+            ;;
+        *)
+            fatal_error "Unknown action=${action}"
+            ;;
+    esac
+}
+
+sync_dir() {
+    if [[ -z $1 ]]; then
+        src=$src_root
+        dst=$dst_root
+    else
+        src=$src_root/$1
+        dst=$dst_root/$1
+    fi
+ 
+    printf "${src} -> ${dst} "
+    if [[ ! -d $src ]]; then
+        printf "- source directory doesn't exist, skipping"
+        return
+    fi
+
+    printf "..."
+    mkdir -p "${dst}"
+    rsync -ah --delete "${src}/" "${dst}/" && printf "\b\b\b- Done\n"
+}
+
+sync_file() {
+    src=$src_root/$1
+    dst=$dst_root/$1
+
+    printf "${src} -> ${dst} "
+    if [[ ! -f $src ]]; then
+        printf "- source file doesn't exist, skipping"
+        return
+    fi
+
+    printf "..."
+    mkdir -p "${dst_root}"
+    cp "${src}" "${dst}" && printf "\b\b\b- Done\n"
+}
 
 
-echo ''
-echo Backing alacrity config ...
-dst=${repo_root}/alacritty
-src=~/.config/alacritty
-if [ ! -d "$src" ]; then
-  echo Skipping - failed to locate config directory $src
-else
-  mkdir -p "${dst}"
-  cp -r "${src}/*" "${dst}/"
-  echo Done
-fi
+# Perform action
+echo "Performing $action ..."
+echo "Config repo: $repo_root"
 
 
-echo ''
-echo Backing nvim config ...
-dst=${repo_root}/nvim
-src=~/.config/nvim
-if [ ! -d "$src" ]; then
-  echo Skipping - failed to locate config directory $src
-else
-  mkdir -p "${dst}"
-  cp "${src}/init.lua" "${dst}/"
-  cp -r "${src}/lua" "${dst}/"
-  echo Done
-fi
+cfg_sync_begin 'Alacrity'
+set_dir        '~/.config/alacritty'
+sync_dir
+cfg_sync_end
 
 
-echo ''
-echo Backing up zsh config ...
-dst=${repo_root}/zsh
-src=~
-if [ ! -d "$src" ]; then
-  echo Skipping - failed to locate config directory $src
-else
-  mkdir -p "${dst}"
-  cp "${src}/.zshrc" "${dst}/zshrc"
-  cp "${src}/.p10k.zsh" "${dst}/p10k.zsh"
-  echo Done
-fi
+cfg_sync_begin 'Neofetch'
+set_dir        '~/.config/neofetch'
+sync_dir
+cfg_sync_end
+
+
+cfg_sync_begin 'Neovim'
+set_dir        '~/.config/nvim'
+sync_file      'init.lua'
+sync_dir       'lua'
+cfg_sync_end
+
+
+cfg_sync_begin 'Powerline10k'
+set_dir        '~' 'zsh'
+sync_file      '.p10k.zsh'
+cfg_sync_end
+
+
+cfg_sync_begin 'ZSH'
+set_dir        '~' 'zsh'
+sync_file      '.zshrc'
+cfg_sync_end
 
 
 # Show status
