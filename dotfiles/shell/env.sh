489673cdb62a5f6d67640910e102fd19f86a180d
gonzaless <gonzaless.7@gmail.com>
2025-03-10 16:37:00 -0400

    Fix micromamba integration to work on both MacOS and Linux

--- dotfiles/shell/env.sh
+++ dotfiles/shell/env.sh
@@ -11,6 +11,26 @@ envcfg_add_path_if_exists() {
 envcfg_add_path_if_exists "$HOME/bin" prepend
 envcfg_add_path_if_exists "$HOME/.local/bin" prepend
 
-export MAMBA_ROOT_PREFIX="${HOME}/.mamba"
-export MAMBA_EXE="${HOME}/.local/bin/micromamba"
+setup_mamba_env() {
+    export MAMBA_ROOT_PREFIX="${HOME}/.mamba"
+
+    local p=`which micromamba`
+    if [[ -f $p ]]; then
+       export MAMBA_EXE="$p"
+    fi
+
+    local possible_paths=(
+        "${HOME}/.local/bin/micromamba"
+        "/usr/local/bin/micromamba"
+    )
+
+    for p in "${possible_paths[@]}" ; do
+        if [[ -f $p ]]; then
+            export MAMBA_EXE="$p"
+            return
+        fi
+    done
+}
+
+setup_mamba_env
 
