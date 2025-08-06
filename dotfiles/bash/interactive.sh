8895c6f680fb647ff23926bf7e39a2dd258cf90a
gonzaless <gonzaless.7@gmail.com>
2025-08-06 17:27:13 +0000

    Customize bash prompt (DIY version)

--- dotfiles/bash/interactive.sh
+++ dotfiles/bash/interactive.sh
@@ -2,6 +2,49 @@ source "$HOME/.dotfiles/shell/interactive.sh"
 
 
 ###############################################################################
+# Prompt
+###############################################################################
+prompt_command() {
+    local last_ret=$?
+
+    local RESET="\[$(tput sgr0)\]"
+    local BLUE="\[$(tput setaf 4)\]"
+    local CIAN="\[$(tput setaf 6)\]"
+    local GREEN="\[$(tput setaf 2)\]"
+    local RED="\[$(tput setaf 1)\]"
+    local YELLOW="\[$(tput setaf 3)\]"
+
+    if [[ $last_ret == 0 ]]; then
+        local carrot="${GREEN}>"
+    else
+        local carrot="${RED} ${last_ret}>"
+    fi
+
+    if [[ -z $CONDA_PREFIX ]]; then
+        local conda_info=""
+    else
+        local conda_info="(${CONDA_PREFIX##*/}) "
+    fi
+
+    if git rev-parse --is-inside-work-tree &> /dev/null ; then
+        local git_branch="$( git rev-parse --abbrev-ref HEAD )"
+        if git status --porcelain &>/dev/null ; then
+            local git_info="${BLUE} ${git_branch}"
+        else
+            local git_info="${YELLOW} *${git_branch}"
+        fi
+    else
+        local git_info=""
+    fi
+
+    export PS1="${conda_info}${GREEN}${HOSTNAME%%.huron*}:${CIAN}\w${git_info}${carrot}${RESET} "
+}
+
+export PROMPT_DIRTRIM=2
+PROMPT_COMMAND=prompt_command
+
+
+###############################################################################
 # Key Bindings
 ###############################################################################
 bind '"\e[A": history-search-backward'
